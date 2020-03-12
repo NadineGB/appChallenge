8bf84e1b53f91412768b2ff96ed6b289c1cb0753
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-12 16:19:11 +0100

    Zweiter Commit

--- app/src/main/java/com/example/appchallenge/MainActivity.java
+++ app/src/main/java/com/example/appchallenge/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.appchallenge;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.buttonchallenge.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void meinClickEvent(View view) {
+        Log.i("info", "Hallo user");
     }
 }
