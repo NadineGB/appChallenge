c3ca1ae2147d59a6244006e0b68b4c9c67d1212c
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-13 12:06:27 +0100

    Image wechselt

--- app/src/main/java/com/example/appchallenge/MainActivity.java
+++ app/src/main/java/com/example/appchallenge/MainActivity.java
@@ -4,11 +4,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.buttonchallenge.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +20,12 @@ public class MainActivity extends AppCompatActivity {
     public void login(View view) {
         EditText usernameET = findViewById(R.id.usernameET);
         EditText passwordET = findViewById(R.id.passwordET);
+        ImageView imgView = findViewById(R.id.logo);
 
         Log.i("Username", usernameET.getText().toString());
         Log.i("Passwort", passwordET.getText().toString());
 
         Toast.makeText(this, "Hallo " + usernameET.getText().toString(), Toast.LENGTH_LONG).show();
+        imgView.setImageResource(R.drawable.cat);
     }
 }
